21ff83119e43704435fde0f2f241a4e33362f61b
rozran00@gmail.com <rozran00@gmail.com>
2022-09-19 10:50:40 -0400

    updated dataset

--- Data Skills Intensive/Tableau/Session 1/create_tidy_data.R
+++ Data Skills Intensive/Tableau/Session 1/create_tidy_data.R
@@ -12,5 +12,19 @@ data <- data %>%
     filter(year != "aggregate") %>% 
     mutate(year = as.numeric(str_remove(year, "y-")))
 
+zip_codes <- read_csv("Data Skills Intensive/Tableau/Session 1/Session 1 - In Class/zip_code_database.csv")
+
+
+data <- data %>% 
+    left_join(zip_codes, by = c("zip_code" = "zip")) %>% 
+    select("zip_code",
+           "year",
+           "population",
+           "primary_city",
+           "state",
+           "county") %>% 
+    rename(city = primary_city)
+
+
 write_csv(data, 
           "Data Skills Intensive/Tableau/Session 1/Session 1 - In Class/pop-by-zip-code-long.csv")
