d6cc5fe0860f78998583acc7783af268531f4074
rozran00@gmail.com <rozran00@gmail.com>
2022-11-02 12:53:02 -0400

    Adding R Session 1

---
+++ Data Skills Intensive/R/Session 1/01_intro_to_r_together.R
@@ -0,0 +1,44 @@
+###############################
+###############################
+##    GOVERN FOR AMERICA     ##
+##       JACOB ROZRAN        ##
+##        2022-11-02         ##
+## INTRODUCTION TO R/RSTUDIO ##
+##  INTERACTIVE ASSIGNMENT   ##
+###############################
+###############################
+
+# Verify that 5 + 5 = 10
+
+
+# Verify that 5^5 = 3125
+
+
+# Verify that 5 - (-5) = 10
+
+
+# Verify that 1 + 2 * 3 = 7 (make sure order of operations works properly)
+
+
+# Verify that rounding the square root of e^10 is 148
+
+
+# Verify that rounding the square root of e^10 to one decimal place is 148.4
+
+
+# Verify that 5 + 6 > 10 using logical operators
+
+
+# Verify that the square root of 25 is 5 using logical operators
+
+
+# Verify that both of the previous two statements are true, 
+# using the & logical operator
+
+
+# Install the package tidyverse and load it 
+
+
+# Install the package mdsr and load it 
+
+
